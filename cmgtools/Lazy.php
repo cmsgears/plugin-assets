a6c21927f6c4d34c81e47bcaaf79537088727978
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2019-03-31 23:10:22 +0530

    Applied quick fixes.

---
+++ cmgtools/Lazy.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * This file is part of CMSGears Framework. Please view License file distributed
+ * with the source code for license details.
+ *
+ * @link https://www.cmsgears.org/
+ * @copyright Copyright (c) 2015 VulpineCode Technologies Pvt. Ltd.
+ */
+
+namespace cmsgears\assets\cmgtools;
+
+// Yii Imports
+use yii\web\View;
+
+/**
+ * Lazy registers the assets specific to lazy loading images.
+ *
+ * @since 1.0.0
+ */
+class Lazy extends \yii\web\AssetBundle {
+
+	// Variables ---------------------------------------------------
+
+	// Globals -------------------------------
+
+	// Constants --------------
+
+	// Public -----------------
+
+	// Protected --------------
+
+	// Variables -----------------------------
+
+	// Public -----------------
+
+	/**
+	 * @inheritdoc
+	 */
+	public $sourcePath = '@bower/cmt-velocity/src';
+
+	// Load Javascript
+	public $js = [
+		// scripts path
+		'solo/lazy.js'
+	];
+
+	// Position to load Javascript
+	public $jsOptions = [
+		'position' => View::POS_END
+	];
+
+	// Protected --------------
+
+	// Private ----------------
+
+	// Traits ------------------------------------------------------
+
+	// Constructor and Initialisation ------------------------------
+
+	// Instance methods --------------------------------------------
+
+	// Yii interfaces ------------------------
+
+	// Yii parent classes --------------------
+
+	// CMG interfaces ------------------------
+
+	// CMG parent classes --------------------
+
+	// Lazy ----------------------------------
+
+}
