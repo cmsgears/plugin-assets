4aeae20fe18bfd19bbd75d5c98607f08270da3e9
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2020-04-15 15:20:10 +0530

    Applied quick fixes.

---
+++ libraries/Konva.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * This file is part of CMSGears Framework. Please view License file distributed
+ * with the source code for license details.
+ *
+ * @link https://www.cmsgears.org/
+ * @copyright Copyright (c) 2015 VulpineCode Technologies Pvt. Ltd.
+ */
+
+namespace cmsgears\assets\libraries;
+
+// Yii Imports
+use yii\web\View;
+
+/**
+ * Konva can be used to load Konva.
+ *
+ * @since 1.0.0
+ */
+class Konva extends \yii\web\AssetBundle {
+
+	// Variables ---------------------------------------------------
+
+	// Globals ----------------
+
+	// Public -----------------
+
+	/**
+	 * @inheritdoc
+	 */
+	public $sourcePath = '@vendor/konvajs/konva';
+
+	/**
+	 * @inheritdoc
+	 */
+	public $js = [
+		'konva.min.js'
+	];
+
+	/**
+	 * @inheritdoc
+	 */
+	public $jsOptions = [
+		'position' => View::POS_END
+	];
+
+	// Protected --------------
+
+	// Private ----------------
+
+	// Traits ------------------------------------------------------
+
+	// Constructor and Initialisation ------------------------------
+
+	/**
+	 * @inheritdoc
+	 */
+    public function init() {
+
+		if( YII_DEBUG ) {
+
+			$this->js = [ 'konva.js' ];
+		}
+    }
+
+	// Instance methods --------------------------------------------
+
+	// Yii interfaces ------------------------
+
+	// Yii parent classes --------------------
+
+	// CMG interfaces ------------------------
+
+	// CMG parent classes --------------------
+
+	// Konva ---------------------------------
+
+}
